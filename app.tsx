412528dbc29b0b3973ff5f9d4864d3cb9ded16f5
benjaminfayaz <benjaminfayaz@gmail.com>
2022-03-04 22:45:11 +0100

    inline emoji favicon

--- app.tsx
+++ app.tsx
@@ -13,7 +13,9 @@ export default function App({ Page, pageProps }: { Page: FC, pageProps: Record<s
         <link rel="stylesheet" href="./style/normalize.css" />
         <link rel="stylesheet" href="./style/app.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;500&family=Righteous&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;500&family=Righteous&display=swap" rel="stylesheet" />
+        {/* copied from https://css-tricks.com/emoji-as-a-favicon/ */}
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🕵️‍♂️</text></svg>"/>
       </head>
       <Page {...pageProps} />
     </main>
