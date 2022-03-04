ad736f0c34407daeeb2c79f696f5a382a28fa60c
benjaminfayaz <benjaminfayaz@gmail.com>
2022-03-04 22:38:09 +0100

    correct imports

--- adapter/web-formatter.ts
+++ adapter/web-formatter.ts
@@ -1,7 +1,7 @@
 import { Formatter, FormatterOnFinishOptions } from 'sherlock/formatters/formatter.ts';
-import Observe from 'https://deno.land/x/Observe@v1.2.1/Observe.ts';
+import Observe from 'observe/Observe.ts';
 import type { SiteResult } from 'sherlock/types.ts';
-import { getSiteUserUrl } from '../sherlock-deno/src/lib/fetcher.ts';
+import { getSiteUserUrl } from 'sherlock/lib/fetcher.ts';
 
 export type WebFormatterOptions = {
   showAll: boolean;
