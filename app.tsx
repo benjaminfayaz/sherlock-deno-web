22788630dd70d283f0e8c8f8107203ea7009af01
benjaminfayaz <benjaminfayaz@gmail.com>
2022-03-05 16:04:55 +0100

    header leftover + rename title

--- app.tsx
+++ app.tsx
@@ -3,12 +3,8 @@ import React, { FC } from 'react'
 export default function App({ Page, pageProps }: { Page: FC, pageProps: Record<string, unknown> }) {
   return (
     <main>
-      <header>
-        <h1>Sherlock Deno Web</h1>
-        <p>Search for users across 270+ websites in your browser</p>
-      </header>
       <head>
-        <title>Sherlock Deno Web</title>
+        <title>Sherlock Web</title>
         <meta name="viewport" content="width=device-width" />
         <link rel="stylesheet" href="./style/normalize.css" />
         <link rel="stylesheet" href="./style/app.css" />
